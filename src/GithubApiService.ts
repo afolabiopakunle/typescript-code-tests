a778d8a60eadb51fc71762aa80422c745ffdd125
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-02-15 08:44:14 +0100

    service added

---
+++ src/GithubApiService.ts
@@ -0,0 +1,13 @@
+import * as request from 'request'
+
+export class GithubApiService {
+    getUserInfo(userName: string) {
+        request.get(`https://api.github.com/users/${userName}`, (response) => {
+            console.log(response);
+        })
+    }
+
+    getRepos() {
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Hello world!");
+import {GithubApiService} from "./GithubApiService";
+
+let svc = new GithubApiService();
+svc.getUserInfo('afolabiopakunle')
