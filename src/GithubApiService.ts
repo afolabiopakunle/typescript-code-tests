ef4dfef4ca3f7e2e6f2d4f79c2bac803aa76b214
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-02-15 09:05:03 +0100

    service class working

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -2,8 +2,14 @@ import * as request from 'request'
 
 export class GithubApiService {
     getUserInfo(userName: string) {
-        request.get(`https://api.github.com/users/${userName}`, (response) => {
-            console.log(response);
+        let options: any = {
+            headers: {
+                'User-Agent': 'request'
+            }
+        }
+        request.get(`https://api.github.com/users/${userName}`, options, (error, response, body) => {
+            console.log(body);
+            // console.log(error);
         })
     }
 
